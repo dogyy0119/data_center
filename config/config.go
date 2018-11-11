7a6fe21b8a447a25d8bb8305d3e70eb92a6e2f00
liuhang <https://gitlab.linkedsee.com>
2018-11-11 23:56:14 +0800

    add config

--- config/config.go
+++ config/config.go
@@ -7,7 +7,20 @@ import (
 	"github.com/naoina/toml"
 )
 
-type Config struct {
+type DataModel struct {
+	itemid 		string
+	type_ 		string
+	hostid		string
+	name 		string
+	key_		string
+	lastvalue 	string
+	lastclock 	string
+	value_type 	string
+	datatype    string
+	description	string
+}
+
+type ZabbixConfig struct {
 	Zabbix ZabbixInfo
 }
 
@@ -18,8 +31,30 @@ type ZabbixInfo struct {
 	Password 	string
 }
 
+type OpenfalconConfig struct {
+	Openfalcon  OpenfalconInfo
+}
 
-func Get_conf() (config Config, err error) {
+type OpenfalconInfo struct {
+	Ip          string
+	Port        string
+	Name        string
+	Password    string
+}
+
+type Database struct {
+	Mysql MysqlInfo
+}
+
+type MysqlInfo struct {
+	Ip          string
+	Port        string
+	Name        string
+	Password    string
+	Tabel		string
+}
+
+func Get_ZabbixConf() (config ZabbixConfig, err error) {
 	configFile := flag.String("c","./app.conf","test comfiguration")
 	flag.Parse()
 
@@ -41,3 +76,47 @@ func Get_conf() (config Config, err error) {
 	return
 }
 
+
+func Get_OpenfalconConf() (config OpenfalconConfig, err error) {
+	configFile := flag.String("c","./app.conf","test comfiguration")
+	flag.Parse()
+
+	f,err := os.Open(*configFile)
+	if err != nil {
+		panic(err)
+		fmt.Println("file error!")
+	}
+
+	defer f.Close()
+
+
+	if err = toml.NewDecoder(f).Decode(&config); err != nil {
+		panic(err)
+		fmt.Println("open file false!")
+		return
+	}
+
+	return
+}
+
+func Get_MysqlConf() (config MysqlInfo, err error) {
+	configFile := flag.String("c","./app.conf","test comfiguration")
+	flag.Parse()
+
+	f,err := os.Open(*configFile)
+	if err != nil {
+		panic(err)
+		fmt.Println("file error!")
+	}
+
+	defer f.Close()
+
+
+	if err = toml.NewDecoder(f).Decode(&config); err != nil {
+		panic(err)
+		fmt.Println("open file false!")
+		return
+	}
+
+	return
+}
